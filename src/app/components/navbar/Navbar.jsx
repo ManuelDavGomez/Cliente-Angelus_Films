699bbaf298f0232a59f13272f6fdf99937a023b0
Manuel Daviid Gomez Sanchez <manueldavidgomez2003@gmail.com>
2025-02-14 23:25:13 -0500

    Terminado la seccion de portafolio con los trabajso que faltaban

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
               src={Logo}
               alt="Picture of the author"
       
-              loading="lazy"
+              loading="eager"
               width={100}
               height={100} 
             />
